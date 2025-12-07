1ae99a425f1197376c1b0e6c38b061b1f476d5d3
ASilentSausage <changer20220415@outlook.com>
2025-12-07 17:03:56 +0800

    最后提交版本:v0.0.251207a
    最后提交版本:v0.0.251207a
    写了最小窗口逻辑,边学边写属于是.

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,38 @@
 #include <SDL3_ttf/SDL_ttf.h>
 #include <SDL3_mixer/SDL_mixer.h>
 #include <SDL3/SDL_main.h>
+#include <iostream>
 
-int main(int argc ,char *argv[])
+int main(int argc, char *argv[])
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Event event;
-    SDL_Init(SDL_INIT_AUDIO|SDL_INIT_EVENTS|SDL_INIT_VIDEO);
-    window=SDL_CreateWindow("Parasite Engine", 0, 0, SDL_WINDOW_RESIZABLE);
-    renderer=SDL_CreateRenderer(window,"Parasite main");
-    while(true)
+    SDL_Init(SDL_INIT_AUDIO | SDL_INIT_EVENTS | SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer("Parasite Engine", 1200, 800, SDL_WINDOW_RESIZABLE, &window, &renderer);
+    std::cout << "Built window&renderer success." << std::endl;
+    bool loop = true;
+    while (loop)
     {
-        while(SDL_PollEvent(&event))
+        while (SDL_PollEvent(&event))
         {
-            if()
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                std::cout << "Event: QUIT" << std::endl;
+                loop = false;
+            }
         }
+
+        if (!SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE))
+        {
+            std::cout << SDL_GetError() << std::endl;
+        }
+        
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
     }
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
