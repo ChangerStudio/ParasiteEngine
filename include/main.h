5d976dab03c6b0bd159da745179cd6b441e52918
ASilentSausage <changer20220415@outlook.com>
2026-01-01 23:19:44 +0800

    0.0.260101-alpha
    Co-Authored-By: ChangerStudio <248178765+ChangerStudio@users.noreply.github.com>

--- include/main.h
+++ include/main.h
@@ -1,8 +1,30 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 
 #include <SDL3/SDL.h>
 #include <SDL3_image/SDL_image.h>
 #include <SDL3_ttf/SDL_ttf.h>
 #include <SDL3_mixer/SDL_mixer.h>
+
+
+#define PROJECT_NAME "Parasite Engine : Parasite Coding Space"
+#define PROJECT_AUTHOR "Changer Studio"
+#define PROJECT_VERSION "0.0.260101-alpha"
+#define PROJECT_WEBSITE "http://crstudio.cloud/ps/pcs"
+
+#define WINDOW_TITLE "Parasite Coding Space"
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
+#define WINDOW_FLAGS SDL_WINDOW_RESIZABLE | SDL_WINDOW_BORDERLESS
+
+/** 
+* SDL初始化时要加载的内容
+**/
+#define INIT_FLAGS SDL_INIT_EVENTS | SDL_INIT_VIDEO
+
+
+const Uint8 BG_R = 25;
+const Uint8 BG_G = 25;
+const Uint8 BG_B = 25;
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,93 @@
 #include <main.h>
 
-SDL_Window *window;
-SDL_Renderer *renderer;
-SDL_Event event;
+SDL_HitTestResult MoveWindowCallback(SDL_Window *win, const SDL_Point *area, void *data)
+{
+    return SDL_HITTEST_DRAGGABLE;
+}
+
+class Window
+{
+private:
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+public:
+    Window(const char *title, int width, int height, SDL_WindowFlags window_flags)
+    {
+        SDL_CreateWindowAndRenderer(title, width, height, window_flags, &window, &renderer);
+        SDL_SetWindowOpacity(window, 0.5f);
+        SDL_SetWindowHitTest(window, MoveWindowCallback, NULL);
+    }
+
+    ~Window()
+    {
+        SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(renderer);
+        std::cout << "Window QUIT." << std::endl;
+    }
+
+    void start_display()
+    {
+        SDL_RenderClear(renderer);
+    }
+
+    void end_display()
+    {
+        SDL_RenderPresent(renderer);
+    }
+
+    void fill_color(Uint8 r, Uint8 g, Uint8 b)
+    {
+        SDL_SetRenderDrawColor(renderer, r, g, b, 0);
+    }
+};
+
+class Main
+{
+private:
+    Window window = Window(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_FLAGS);
+    SDL_Event event;
+
+    bool loop = true;
+
+    void listen()
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                loop = false;
+            }
+        }
+    }
+
+public:
+    Main()
+    {
+        SDL_Init(INIT_FLAGS);
+    }
+
+    void run()
+    {
+        while (loop)
+        {
+            listen();
+            window.start_display();
+            window.fill_color(BG_R, BG_G, BG_G);
+            window.end_display();
+            std::cout << 0;
+        }
+    }
+
+    void quit()
+    {
+        std::cout << "Main QUIT." << std::endl;
+    }
+};
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer("test", 1200, 800, SDL_WINDOW_RESIZABLE, &window, &renderer);
-    bool loop=true;
-    while(loop)
-    {
-        while(SDL_PollEvent(&event))
-        {
-            if(event.type == SDL_EVENT_QUIT)
-            {
-                loop=false;
-            }
-        }
-        SDL_SetRenderDrawColor(renderer, 25, 25, 25, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
-    }
-    SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
+    Main main;
+    main.run();
+    main.quit();
     return 0;
 }
